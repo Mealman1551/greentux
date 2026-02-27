a982382897ed59f26a49779118d858ee684fdb57
Mealman1551 <nathandubuy4@gmail.com>
2026-02-27 13:41:37 +0100

    Update main.py

--- greentux/main.py
+++ greentux/main.py
@@ -4,23 +4,31 @@ import signal
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-os.environ["QT_QPA_PLATFORM"] = "xcb"  # moet voor Qt imports!
+os.environ["QT_QPA_PLATFORM"] = "xcb"
 
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtGui import QIcon
 from .app_window import GreenTuxWindow
 from .tray import create_tray
 from .webview import set_tray
-from .config import TRAY_ICON_PATH
+from .config import resource_path, TRAY_ICON_PATH
 
-app = QApplication(sys.argv)
-app.setWindowIcon(QIcon(TRAY_ICON_PATH))
-window = GreenTuxWindow()
-tray = create_tray(app, window)
-set_tray(tray)
 
-with open("themes/dark.qss") as f:
-    app.setStyleSheet(f.read())
+def main():
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon(TRAY_ICON_PATH))
 
-window.show()
-sys.exit(app.exec())
+    window = GreenTuxWindow()
+    tray = create_tray(app, window)
+    set_tray(tray)
+
+    qss_path = resource_path("themes/dark.qss")
+    with open(qss_path, "r") as f:
+        app.setStyleSheet(f.read())
+
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
